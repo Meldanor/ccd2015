dc3c97309963839d82e74aa5dd52299fb8597efb
Kilian Gärtner <herr.kilian.gaertner@gmail.com>
2015-11-20 13:24:22 +0100

    Add event broadcaster to register and inform observer.
    Add FigureCaptureEvent.

--- src/main/java/jchess/event/Event.java
+++ src/main/java/jchess/event/Event.java
@@ -2,6 +2,9 @@ package jchess.event;
 
 /**
  * The base class for events. Identified by the event type.
+ * <p>
+ * There must be an 1:1 relation between an Event and its EventType. If there are more than one Event using the same
+ * EventType, this can result in some errors with the {@link EventBroadcaster}.
  *
  * @since 18.11.2015
  */
@@ -15,6 +18,13 @@ public abstract class Event {
 
     public EventType getEventType() {
         return eventType;
+    }
+
+    /**
+     * Inform all registered observer about this event.
+     */
+    public void trigger() {
+        EventBroadcaster.triggerEvent(this);
     }
 
     @Override
--- src/main/java/jchess/event/EventType.java
+++ src/main/java/jchess/event/EventType.java
@@ -2,10 +2,14 @@ package jchess.event;
 
 /**
  * The type of the event. Useful to register to an event.
+ * <p>
+ * There must be an 1:1 relation between an Event and its EventType. If there are more than one Event using the same
+ * EventType, this can result in some errors with the {@link EventBroadcaster}
  *
  * @since 18.11.2015
  */
 public enum EventType {
 
-    FIGURE_MOVED
+    FIGURE_MOVED,
+    FIGURE_CAPTURED
 }
