0e4668aad88d47bfb9cba77f3ea7c0b806080d1e
Krorsok <erik.daenicke@st.ovgu.de>
2015-12-01 22:27:33 +0100

    Add another test

--- src/test/java/jchess/game/movement/PawnMovementTest.java
+++ src/test/java/jchess/game/movement/PawnMovementTest.java
@@ -28,9 +28,9 @@ public class PawnMovementTest {
         figureSetter.putFigure(Position2D.of(6, 11), GameboardCreator.FigureSetup.pawn(HexagonalPlayerType.GRAY));
 
         // The three figures the gray pawn can catch.
-        figureSetter.putFigure(Position2D.of(4, 10), GameboardCreator.FigureSetup.pawn(HexagonalPlayerType.WHITE));
-        figureSetter.putFigure(Position2D.of(5, 9), GameboardCreator.FigureSetup.pawn(HexagonalPlayerType.BLACK));
-        figureSetter.putFigure(Position2D.of(7, 10), GameboardCreator.FigureSetup.pawn(HexagonalPlayerType.WHITE));
+        figureSetter.putFigure(Position2D.of(4, 10), GameboardCreator.FigureSetup.knight(HexagonalPlayerType.WHITE));
+        figureSetter.putFigure(Position2D.of(5, 9), GameboardCreator.FigureSetup.knight(HexagonalPlayerType.BLACK));
+        figureSetter.putFigure(Position2D.of(7, 10), GameboardCreator.FigureSetup.knight(HexagonalPlayerType.WHITE));
 
         HexagonalGameboard gameboard = figureSetter.createGameboard();
 
@@ -47,6 +47,50 @@ public class PawnMovementTest {
         Figure grayPawn = gameboard.getFigure(Position2D.of(6, 11)).get();
         possibleActions = pawnMoves.getPossibleActions(grayPawn, gameboard);
         assertEquals(2 + 2 + 1 + 1 + 1, possibleActions.size());
+    }
+
+
+    @Test
+    public void testStandardPawnMovements() {
+        GameboardCreator.FigureSetter figureSetter = new GameboardCreator.FigureSetter();
+
+        // A white pawn at the border of the gameboard.
+        figureSetter.putFigure(Position2D.of(2, 0), GameboardCreator.FigureSetup.pawn(HexagonalPlayerType.WHITE));
+
+        // A figure the white pawn can catch.
+        figureSetter.putFigure(Position2D.of(3, 2), GameboardCreator.FigureSetup.bishop(HexagonalPlayerType.BLACK));
+
+        // A black pawn.
+        figureSetter.putFigure(Position2D.of(7, 4), GameboardCreator.FigureSetup.pawn(HexagonalPlayerType.BLACK));
+
+        // A gray pawn.
+        figureSetter.putFigure(Position2D.of(3, 7), GameboardCreator.FigureSetup.pawn(HexagonalPlayerType.GRAY));
+
+        // Three figures the gray pawn could(!) catch.
+        figureSetter.putFigure(Position2D.of(1, 6), GameboardCreator.FigureSetup.rook(HexagonalPlayerType.WHITE));
+        figureSetter.putFigure(Position2D.of(2, 5), GameboardCreator.FigureSetup.rook(HexagonalPlayerType.BLACK));
+        figureSetter.putFigure(Position2D.of(4, 6), GameboardCreator.FigureSetup.queen(HexagonalPlayerType.WHITE));
+
+        // To figures blocking the way for the gray pawn to catch the white rook. Also disables the pawn to do one of
+        // his normal moves.
+        figureSetter.putFigure(Position2D.of(2, 6), GameboardCreator.FigureSetup.rook(HexagonalPlayerType.GRAY));
+        figureSetter.putFigure(Position2D.of(2, 7), GameboardCreator.FigureSetup.knight(HexagonalPlayerType.WHITE));
+
+        HexagonalGameboard gameboard = figureSetter.createGameboard();
+
+        PawnMovement pawnMoves = new PawnMovement();
+
+        Figure whitePawn = gameboard.getFigure(Position2D.of(2, 0)).get();
+        List<ChessAction> possibleActions = pawnMoves.getPossibleActions(whitePawn, gameboard);
+        assertEquals(1 + 1 + 1, possibleActions.size());
+
+        Figure blackPawn = gameboard.getFigure(Position2D.of(7, 4)).get();
+        possibleActions = pawnMoves.getPossibleActions(blackPawn, gameboard);
+        assertEquals(1 + 1, possibleActions.size());
+
+        Figure grayPawn = gameboard.getFigure(Position2D.of(3, 7)).get();
+        possibleActions = pawnMoves.getPossibleActions(grayPawn, gameboard);
+        assertEquals(1 + 0 + 1 + 1, possibleActions.size());
     }
 
 }
