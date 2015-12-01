228f31504ba6a28d73ad7ed858840f65b67ef5cd
Krorsok <erik.daenicke@st.ovgu.de>
2015-12-01 21:33:26 +0100

    Add class for simplifying the creation of test cases

---
+++ src/test/java/jchess/game/GameboardCreator.java
@@ -0,0 +1,137 @@
+package jchess.game;
+
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * This class creates the {@link DefaultHexagonalGameboard} without automatically setting the figures. The developer
+ * gets the opportunity to set the figures on his own. This way, the creation of test cases for unit tests will be
+ * simplified. FOR TESTING ONLY.
+ *
+ * @since 28.11.2015.
+ */
+public class GameboardCreator extends HexagonalGameboard {
+
+    private static final Map<Position2D, Position2D> rows;
+
+    static {
+        Map<Position2D, Position2D> rowsMap = new HashMap<>();
+        rowsMap.put(Position2D.of(0, 0), Position2D.of(0, 7));
+        rowsMap.put(Position2D.of(1, 0), Position2D.of(1, 8));
+        rowsMap.put(Position2D.of(2, 0), Position2D.of(2, 9));
+        rowsMap.put(Position2D.of(3, 0), Position2D.of(3, 10));
+        rowsMap.put(Position2D.of(4, 0), Position2D.of(4, 11));
+        rowsMap.put(Position2D.of(5, 0), Position2D.of(5, 12));
+        rowsMap.put(Position2D.of(6, 1), Position2D.of(6, 12));
+        rowsMap.put(Position2D.of(7, 2), Position2D.of(7, 12));
+        rowsMap.put(Position2D.of(8, 3), Position2D.of(8, 12));
+        rowsMap.put(Position2D.of(9, 4), Position2D.of(9, 12));
+        rowsMap.put(Position2D.of(10, 5), Position2D.of(10, 12));
+        rowsMap.put(Position2D.of(11, 6), Position2D.of(11, 12));
+        rowsMap.put(Position2D.of(12, 7), Position2D.of(12, 12));
+        rows = Collections.unmodifiableMap(rowsMap);
+    }
+
+
+    /**
+     * Create a {@link HexagonalGameboard} including figures set by the user.
+     *
+     * @param figures The figures and their respective positions.
+     */
+    private GameboardCreator(Map<Position2D, Figure> figures) {
+        super(rows, figures);
+    }
+
+
+    /**
+     * Used for setting figures (including their respective positions) and getting a gameboard containing these figures
+     * at the specified positions.
+     */
+    public static class FigureSetter {
+
+        private Map<Position2D, Figure> figuresMap;
+
+        /**
+         * Holds all the figures and their respective positions, that can be set by the user.
+         */
+        public FigureSetter() {
+            figuresMap = new HashMap<>();
+        }
+
+
+        /**
+         * @return A Map of figures and their positions.
+         */
+        public Map<Position2D, Figure> getFiguresMap() {
+            return figuresMap;
+        }
+
+
+        /**
+         * @param figuresMap A Map of figures and their positions.
+         */
+        public void setFiguresMap(Map<Position2D, Figure> figuresMap) {
+            this.figuresMap = figuresMap;
+        }
+
+
+        /**
+         * Specify a figure and its position on a gameboard.
+         *
+         * @param position Position of the figure.
+         * @param figure   The figure itself.
+         */
+        public void putFigure(Position2D position, Figure figure) {
+            figuresMap.put(position, figure);
+        }
+
+
+        /**
+         * Get the gameboard including all the figures previously set by
+         * {@link #putFigure(Position2D, Figure)} or {@link #setFiguresMap(Map)}.
+         *
+         * @return A {@link HexagonalGameboard} that may include figures.
+         */
+        public HexagonalGameboard createGameboard() {
+            return new GameboardCreator(Collections.unmodifiableMap(figuresMap));
+        }
+    }
+
+
+    /**
+     * Used for fast creation of a figure.
+     */
+    public static class FigureSetup extends Figure {
+
+        public FigureSetup(String name, FigureType type, HexagonalPlayerType player) {
+            super(name, type, player);
+        }
+
+        public static Figure pawn(HexagonalPlayerType player) {
+            return new FigureSetup("Pawn", FigureType.PAWN, player);
+        }
+
+        public static Figure rook(HexagonalPlayerType player) {
+            return new FigureSetup("Rook", FigureType.ROOK, player);
+        }
+
+        public static Figure bishop(HexagonalPlayerType player) {
+            return new FigureSetup("Bishop", FigureType.BISHOP, player);
+        }
+
+        public static Figure knight(HexagonalPlayerType player) {
+            return new FigureSetup("Knight", FigureType.KNIGHT, player);
+        }
+
+        public static Figure queen(HexagonalPlayerType player) {
+            return new FigureSetup("Queen", FigureType.QUEEN, player);
+        }
+
+        public static Figure king(HexagonalPlayerType player) {
+            return new FigureSetup("King", FigureType.KING, player);
+        }
+
+    }
+
+}
