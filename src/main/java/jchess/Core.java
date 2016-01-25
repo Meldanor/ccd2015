27afe1c08cd052ab65eea04616f4f78d3b5e36f9
Kilian Gärtner <herr.kilian.gaertner@gmail.com>
2016-01-25 15:41:40 +0100

    Implement programs start

--- src/main/java/jchess/Core.java
+++ src/main/java/jchess/Core.java
@@ -1,5 +1,9 @@
 package jchess;
 
+import jchess.game.DefaultHexagonalGameboard;
+import jchess.game.HexagonalGameboard;
+import jchess.ui.HexagonalGameboardGUI;
+
 /**
  * The entrance of the program.
  * @since 02.12.2015
@@ -7,5 +11,11 @@ package jchess;
 public class Core {
     public static void main(String[] args) {
         System.out.println("Hello World, this is JChess 2.0 by Dash!");
+
+        HexagonalGameboardGUI gui = new HexagonalGameboardGUI();
+
+        HexagonalGameboard gameboard = new DefaultHexagonalGameboard();
+        gui.drawGameboardState(gameboard);
+
     }
 }
